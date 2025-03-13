f9b97d34c555b758e073987be13a842b9df6ccf3
T. Csaba <kyletaylorrules94@gmail.com>
2025-03-13 09:27:04 +0100

    refactored loginview, created registerview

---
+++ Instapp/View/Auth/AuthButton.swift
@@ -0,0 +1,18 @@
+//
+//  AuthButton.swift
+//  Instapp
+//
+//  Created by Turdesan Csaba on 2025. 03. 13..
+//
+
+import SwiftUI
+
+struct AuthButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthButton()
+}
---
+++ Instapp/View/Auth/AuthNavigator.swift
@@ -0,0 +1,18 @@
+//
+//  AuthNavigator.swift
+//  Instapp
+//
+//  Created by Turdesan Csaba on 2025. 03. 13..
+//
+
+import SwiftUI
+
+struct AuthNavigator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthNavigator()
+}
